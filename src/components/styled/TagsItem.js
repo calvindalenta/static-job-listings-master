28d27043d81f065fa0e4da20d98ea6007c9bf427
Calvin Dalenta <calvindalentark@gmail.com>
2021-10-03 03:32:18 +0700

    Change font-size and padding

--- src/components/styled/TagsItem.js
+++ src/components/styled/TagsItem.js
@@ -2,7 +2,8 @@ import styled from "styled-components";
 
 const TagsItem = styled.div`
     cursor: pointer;
-    padding: 0.3em;
+    font-size: 0.9em;
+    padding: 0.4em;
     background-color: ${({theme}) => theme.tabletBackground};
     color: hsl(180, 29%, 50%);
     font-weight: bold;
