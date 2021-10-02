3f6d9095447dd8df2635fd3176f14ecf333dc649
Calvin Dalenta <calvindalentark@gmail.com>
2021-10-03 03:22:47 +0700

    Fix CardPosition text color

--- src/components/styled/CardPosition.js
+++ src/components/styled/CardPosition.js
@@ -2,7 +2,11 @@ import styled from "styled-components";
 
 const CardPosition = styled.h3`
     cursor: pointer;
-    color: ${({theme}) => theme.primary}; 
+    color: ${({theme}) => theme.primaryDark}; 
+
+    &:hover {
+        color: ${({theme}) => theme.primary}
+    }
 `;
 
 export default CardPosition;
